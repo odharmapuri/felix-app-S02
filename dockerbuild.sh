350bdb489b2aae53a5b57f99a47323dd8885ecc9
odharmapuri <odharmapuri@gmail.com>
2023-09-27 17:18:23 +0530

    next

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+git clone -b master https://gitlab.com/odharmapuri1/felix-app2.git
+mv felix-app2/* .
+mvn clean install
 docker build -t felix-app -f dockerapp .
 docker build -t felix-db -f dockerdb .
 docker build -t felix-web -f dockerweb .
+docker pull rabbitmq
+docker pull memcached
+docker compose up -d
