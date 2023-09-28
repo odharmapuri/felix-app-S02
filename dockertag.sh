469b00b8ea889ec5c818bf8cbdc2d01d1c5cd9a9
odharmapuri <odharmapuri@gmail.com>
2023-09-28 18:58:14 +0530

    next1

--- dockertag.sh
+++ dockertag.sh
@@ -1,3 +1,3 @@
-docker tag ${project}-app ${docker_repo}/${project}-app
-docker tag ${project}-db ${docker_repo}/${project}-db
-docker tag ${project}-web ${docker_repo}/${project}-web
+docker tag ${project}-app ${docker_repo}/${project}-app:v1
+docker tag ${project}-db ${docker_repo}/${project}-db:v1
+docker tag ${project}-web ${docker_repo}/${project}-web:v1
