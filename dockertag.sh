9ab35ab419c1ed6e3a93a348d9a9c82bbf27f49f
odharmapuri <odharmapuri@gmail.com>
2023-11-01 20:19:00 +0530

    updated ec2.sh

--- dockertag.sh
+++ dockertag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 project="felix"
 docker_repo="odharmapuri"
-version = "v1"
+version="v1"
 docker tag ${project}-app ${docker_repo}/${project}-app:${version}
 docker tag ${project}-db ${docker_repo}/${project}-db:${version}
 docker tag ${project}-web ${docker_repo}/${project}-web:${version}
--- extra files/ec2.sh
+++ extra files/ec2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo apt update -y
+sudo apt update
 #install java and maven
 sudo apt install default-jdk maven git lynx -y
 
 #Install Docker
 #add docker official gpg key
-sudo apt update -y
-sudo apt-get install ca-certificates curl gnupg
+sudo apt update
+sudo apt install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -16,7 +16,7 @@ echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update -y
+sudo apt update
 
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker ubuntu
