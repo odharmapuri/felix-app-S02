fd8d96601aceef5d6b837ddd91fd5e492adb7dba
odharmapuri <odharmapuri@gmail.com>
2023-11-01 19:58:24 +0530

    updated shell scripts

--- dockertag.sh
+++ dockertag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-project = "felix"
-docker_repo = "odharmapuri"
+project="felix"
+docker_repo="odharmapuri"
 version = "v1"
 docker tag ${project}-app ${docker_repo}/${project}-app:${version}
 docker tag ${project}-db ${docker_repo}/${project}-db:${version}
--- extra files/ec2.sh
+++ extra files/ec2.sh
@@ -5,7 +5,7 @@ sudo apt install default-jdk maven git lynx -y
 
 #Install Docker
 #add docker official gpg key
-sudo apt-get update
+sudo apt update -y
 sudo apt-get install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -16,9 +16,9 @@ echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt update -y
 
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker ubuntu
 sudo systemctl restart docker
 
@@ -27,8 +27,8 @@ sudo mv felix-app2/* .
 sudo mvn clean install
 sudo chmod u+x dockerbuild.sh
 sudo chmod u+x dockertag.sh
-./dockerbuild.sh
-./dockertag.sh
+sudo ./dockerbuild.sh
+sudo ./dockertag.sh
 #sudo docker build -t felix-app -f dockerapp .
 #sudo docker build -t felix-db -f dockerdb .
 #sudo docker build -t felix-web -f dockerweb .
